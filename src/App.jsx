eb6d943951d1584602acf9816404b386a96d164e
tcardoso26 <thiago.cardoso26@outlook.com>
2024-06-19 16:52:23 -0300

    iteração no jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,46 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+// author: {avatar_utl: "", name: ""}
+// publishdAt: Date
+// content: String
+
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/tcardoso26",
+      name: "Thiago Cardoso",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa" },
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifolio. É um projeto que eu fiz nmo NLW !!" },
+      { type: "link", content: "thiago.cardoso/timeDev"},
+ 
+    ],
+    publishedAt: new Date("2024-06-06 20:00:00"),
+  }, //array dentro de outro array
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Paulo Coelho",
+      role: "EDUCADOR @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa" },
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifolio. É um projeto que eu fiz nmo NLW !!" },
+      { type: "link", content: "thiago.cardoso/timeDev"},
+ 
+    ],
+    publishedAt: new Date("2024-06-06 20:00:00"),
+  }, //array dentro de outro array
+];
+
+// iteração = repetir alguma coisa, criar uma estrutura de repetição
+
 export function App() {
   return (
     <div>
